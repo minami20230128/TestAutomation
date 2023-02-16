e553852759d03bbee646948fa9d30ae220fdb747
minami20230128 <south614fruit@yahoo.co.jp>
2023-02-17 01:39:33 +0900

    findElseIf追加

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 
 void read(std::string& filepath);
 void write(std::string& filepath);
+void findIf(std::string line);
 std::vector<std::string> conditionals;
 
 int main()
@@ -28,33 +29,8 @@ void read(std::string& filepath)
 
     while(std::getline(file, line))
     {
-        const char* pLine;
-        pLine = line.c_str();
-        bool IsI = false;
-        
-        while(pLine != NULL)
-        {
-            int i = 0;
-            printf("%d¥n", *pLine);
-            if(pLine[i] == 'i')
-            {
-                IsI = true;
-                pLine++;
-                continue;
-            }
-            if(IsI && pLine[i] == 'f')
-            {
-                conditionals.push_back(pLine - 1);
-                break;
-            }
-            if(*pLine != char(0x09) && *pLine != char(0x20))
-            {
-                break;
-            }
-            pLine++;
-            i++;
-        }
-        
+        findIf(line);
+        findElseIf(line);
     }
 }
 
@@ -64,5 +40,44 @@ void write(std::string& filepath)
     for(auto&& conditional : conditionals)
     {
         file << conditional << std::endl;
+    }
+}
+
+void findIf(std::string line)
+{
+    const char* pLine;
+    pLine = line.c_str();
+    bool IsI = false;
+    
+    while(pLine != NULL)
+    {
+        int i = 0;
+        printf("%d¥n", *pLine);
+        if(pLine[i] == 'i')
+        {
+            IsI = true;
+            pLine++;
+            continue;
+        }
+        if(IsI && pLine[i] == 'f')
+        {
+            conditionals.push_back(pLine - 1);
+            break;
+        }
+        if(*pLine != char(0x09) && *pLine != char(0x20))
+        {
+            break;
+        }
+        pLine++;
+        i++;
+    }
+}
+
+void findElseIf(std::string line)
+{
+    auto idx = line.find("else if");
+    if(idx != std::string::npos)
+    {
+        conditionals.push_back(line.substr(idx, line.length() - idx));
     }
 }
