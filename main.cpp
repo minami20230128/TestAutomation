b9d65458a07c9dab1b94611a38c5af8223024438
minami20230128 <south614fruit@yahoo.co.jp>
2023-03-01 23:15:40 +0900

    メソッド名書き出す機能作成中

--- main.cpp
+++ main.cpp
@@ -35,8 +35,21 @@ int main()
     excel = std::make_unique<Excel>();
     excel->createFile(writefilepath);
     read();
+    auto itr = conditionals.begin();
+    while(itr != conditionals.end())
+    {
+        std::cout << itr->first << ":" << itr->second << std::endl;
+        itr++;
+    }
+
+    auto methodItr = methods.begin();
+    while(methodItr != methods.end())
+    {
+        std::cout << methodItr->first << ":" << methodItr->second << std::endl;
+        methodItr++;
+    }
     writeHeader();
-    writeMethod();
+    //writeMethod();
     writeExcel();
 
     return 0;
@@ -46,7 +59,7 @@ void read()
 {
     std::string line;
     std::ifstream file(readfilepath);
-    int linenumber = 0;
+    int linenumber = 1;
 
     while(std::getline(file, line))
     {
@@ -64,25 +77,23 @@ void read()
 
 void writeExcel()
 {
-    int row = DATAROW;
-
     auto itr = conditionals.begin();
-    while(itr != conditionals.end())
-    {
-        if(itr->second == "else")
-        {
-            excel->writeCell(itr->second, row, "D");
-            row += 1;
-        }
-        else
-        {
-            excel->writeCell(itr->second, row, "D");
-            excel->writeCell("true", row, "E");
-            excel->writeCell("false", row + 1, "E");
-            row += 2;
-        }
-        itr++;
-    }
+    auto methodItr = methods.begin();
+    int row = DATAROW;
+    excel->writeCell(methodItr->second, row, "C");
+
+    // while(itr != conditionals.end() && methodItr != methods.end())
+    // {
+    //     excel->writeCell(methodItr->second, row, "C");
+    //     if(methodItr != methods.end()) methodItr++;
+
+    //     if(itr->first < methodItr->first)
+    //     {
+    //         excel->writeCell(itr->second, row, "D");
+    //         itr++;
+    //     }
+    //     row++;
+    // }
 
     int idx = readfilepath.rfind("/");
     std::string modulename = readfilepath.substr(idx + 1);
@@ -98,7 +109,6 @@ void writeMethod()
     auto methodItr = methods.begin();
     while(itr != conditionals.end())
     {
-        std::cout << methodItr->second << std::endl;
         if(itr->first >= methodItr->first)//条件式の行数がメソッドの行数より大きかったら
         {
             excel->writeCell(itr->second, DATAROW, "C");
@@ -135,6 +145,16 @@ void findMethod(std::string& line, int linenumber)
 
     std::string str(pLine); 
     if(str.find("class") != std::string::npos)
+    {
+        return;
+    }
+
+    if(str.find("if") != std::string::npos)
+    {
+        return;
+    }
+
+    if(str.find("while") != std::string::npos)
     {
         return;
     }
